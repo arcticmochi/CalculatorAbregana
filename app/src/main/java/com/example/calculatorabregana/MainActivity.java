5e5d512827335681043c7f38bda7e44da91b7cc0
Laurence_A <abre.svnoj@gmail.com>
2022-07-04 18:12:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/calculatorabregana/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.calculatorabregana;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        digitBtnListenerMethod();
+    }
+
+    public void digitBtnListenerMethod() {
+        View.OnClickListener digitListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText etNumInput = (EditText) findViewById(R.id.etNumInput);
+                Button btn = (Button) view;
+                etNumInput.setText(etNumInput.getText() + "" + btn.getText());
+            }
+        };
+
+        Button btn0 = (Button) findViewById(R.id.btn0);
+        Button btn1 = (Button) findViewById(R.id.btn1);
+        Button btn2 = (Button) findViewById(R.id.btn2);
+        Button btn3 = (Button) findViewById(R.id.btn3);
+        Button btn4 = (Button) findViewById(R.id.btn4);
+        Button btn5 = (Button) findViewById(R.id.btn5);
+        Button btn6 = (Button) findViewById(R.id.btn6);
+        Button btn7 = (Button) findViewById(R.id.btn7);
+        Button btn8 = (Button) findViewById(R.id.btn8);
+        Button btn9 = (Button) findViewById(R.id.btn9);
+
+        btn0.setOnClickListener(digitListener);
+        btn1.setOnClickListener(digitListener);
+        btn2.setOnClickListener(digitListener);
+        btn3.setOnClickListener(digitListener);
+        btn4.setOnClickListener(digitListener);
+        btn5.setOnClickListener(digitListener);
+        btn6.setOnClickListener(digitListener);
+        btn7.setOnClickListener(digitListener);
+        btn8.setOnClickListener(digitListener);
+        btn9.setOnClickListener(digitListener);
+
+    }
+
+
+}
